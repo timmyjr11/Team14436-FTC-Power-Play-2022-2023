b2437a057fc27f10905a6e88891dd61ee4c3f158
Roaring Robotics <roaringrobotics14436@gmail.com>
2022-10-08 13:02:39 -0700

    The start of many codes to come

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/init.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class init extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addLine("The start to many codes");
+        waitForStart();
+    }
+}
